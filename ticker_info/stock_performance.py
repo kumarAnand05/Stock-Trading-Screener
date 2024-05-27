ed4fe44fb0c21cb366a0c3c196a8595432e1b2fa
Anand <kumar.anand05@yahoo.com>
2024-05-28 00:16:47 +0530

    fixed f-string issue

--- ticker_info/stock_performance.py
+++ ticker_info/stock_performance.py
@@ -11,22 +11,22 @@ def get_stock_performance(stock_data):
     avg_10day_movement = round((np.sum(stock_data.loc['High'][:10]) - np.sum(stock_data.loc['Low'][:10])) / 10, 2)
 
     try:
-        weekly_return = f'{round(((cmp * 100) / stock_data.loc['Close'].iloc[6]) - 100, 2)}%'
+        weekly_return = f"{round(((cmp * 100) / stock_data.loc['Close'].iloc[6]) - 100, 2)}%"
     except IndexError:
         weekly_return = 'NA'
 
     try:
-        monthly_return = f'{round(((cmp * 100) / stock_data.loc['Close'].iloc[31]) - 100, 2)}%'
+        monthly_return = f"{round(((cmp * 100) / stock_data.loc['Close'].iloc[31]) - 100, 2)}%"
     except IndexError:
         monthly_return = 'NA'
 
     try:
-        half_yearly_return = f'{round(((cmp * 100) / stock_data.loc['Close'].iloc[181]) - 100, 2)}%'
+        half_yearly_return = f"{round(((cmp * 100) / stock_data.loc['Close'].iloc[181]) - 100, 2)}%"
     except IndexError:
         half_yearly_return = 'NA'
 
     try:
-        yearly_return = f'{round(((cmp * 100) / stock_data.loc['Close'].iloc[365]) - 100, 2)}%'
+        yearly_return = f"{round(((cmp * 100) / stock_data.loc['Close'].iloc[365]) - 100, 2)}%"
     except IndexError:
         yearly_return = 'NA'
 
